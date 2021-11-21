c3c8ba2ccc6adf520651d730a17556dc2f7e4147
PrinceAutumn <841679171@qq.com>
2021-11-21 17:09:01 +0800

    Initial commit

---
+++ src/main/java/com/example/ChuanyiExaminationtest/ApplicationPath.java
@@ -0,0 +1,4 @@
+package com.example.ChuanyiExaminationtest;
+
+public @interface ApplicationPath {
+}
---
+++ src/main/java/com/example/ChuanyiExaminationtest/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.ChuanyiExaminationtest;
+
+import javafx.application.Application;
+
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
